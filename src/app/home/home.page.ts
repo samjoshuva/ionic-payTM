1fe13981c0799470c68a7ae0cbc10b32c8b81dea
sam joshuva <sam613263@gmail.com>
2019-08-20 19:08:24 +0530

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+declare var paytm: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss']
 })
 export class HomePage {
+    amount: Number;
+    email: String;
+    number: Number;
+    constructor(private platform: Platform) {}
 
-  constructor() {}
+    pay() {
+        let options = {
+            ENVIRONMENT: 'staging', // environment details. staging for test environment & production for live environment
+            REQUEST_TYPE: 'DEFAULT', // You would get this details from paytm after opening an account with them
+            MID: 'endlnS53471586868878', // You would get this details from paytm after opening an account with them
+            ORDER_ID: 'ORDER0000000001', // Unique ID for each transaction. This info is for you to track the transaction details
+            CUST_ID: '10000988111', // Unique ID for your customer
+            INDUSTRY_TYPE_ID: 'Retail', // You would get this details from paytm after opening an account with them
+            CHANNEL_ID: 'WAP', // You would get this details from paytm after opening an account with them
+            TXN_AMOUNT: '1', // Transaction amount that has to be collected
+            WEBSITE: 'APPSTAGING', // You would get this details from paytm after opening an account with them
+            CALLBACK_URL:
+                'https://securegw.paytm.in/theia/paytmCallback?ORDER_ID=ORDER0000000001', // Callback url
+            // EMAIL: this.email, // Email of customer
+            MOBILE_NO: 8838558147 // Mobile no of customer
+        };
 
+        this.platform.ready().then(() => {
+            paytm.startPayment(
+                options,
+                this.successCallback,
+                this.failureCallback
+            );
+        });
+    }
+
+    successCallback(response) {
+        if (response.STATUS == 'TXN_SUCCESS') {
+            var txn_id = response.TXNID;
+            var paymentmode = response.PAYMENTMODE;
+            // other details and function after payment transaction
+        } else {
+            // error code will be available in RESPCODE
+            // error list page https://docs.google.com/spreadsheets/d/1h63fSrAmEml3CYV-vBdHNErxjJjg8-YBSpNyZby6kkQ/edit#gid=2058248999
+            alert('Transaction Failed for reason ' + response.RESPMSG);
+        }
+    }
+
+    failureCallback(error) {
+        // error code will be available in RESCODE
+        // error list page https://docs.google.com/spreadsheets/d/1h63fSrAmEml3CYV-vBdHNErxjJjg8-YBSpNyZby6kkQ/edit#gid=2058248999
+        alert('Transaction Failed for reason ' + error.RESPMSG);
+    }
 }
